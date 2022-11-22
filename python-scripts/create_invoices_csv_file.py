87ace2463f2aeefc59dffece25609cf23d04b0bf
dusannistoran <dulekiki@gmail.com>
2022-11-22 11:04:05 +0100

    clean up the mess

--- python-scripts/create_invoices_csv_file.py
+++ python-scripts/create_invoices_csv_file.py
@@ -1,42 +1,22 @@
-"""
-Server logs will come on HDFS each hour. ...
-The data will come as lines of text (RDDs). RDDs will have Invoice No, Stock code, Quantity, Invoice
-date, CustomerID, and Country.
-"""
 import csv
+import configparser
 
-#data_edited_csv_filepath = '/home/dusan/PycharmProjects/JDP_Data_Engineering_Task/venv/csvs/data_edited2.csv'
-data_edited_csv_filepath = 'file:///home/scala/src/main/resources/csvs/data_edited.csv'
-#logs_csv_filepath = '/home/dusan/PycharmProjects/JDP_Data_Engineering_Task/venv/csvs/logs2.csv'
-logs_csv_filepath = 'file:///home/scala/src/main/resources/csvs/invoices.csv'
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-data_logs_dict = {
-    "InvoiceNo": [],
-    "StockCode": [],
-    "Quantity": [],
-    "InvoiceDate": [],
-    "CustomerID": [],
-    "Country": []
-}
+data_edited_csv_filepath = config['data_edited']['path']
+invoices_csv_filepath = config['invoices']['path']
 
-with open(f'{data_edited_csv_filepath}', 'r', encoding="ISO-8859-1") as csv_file:
-    csv_reader = csv.DictReader(csv_file, delimiter=',')
-    for line in csv_reader:
-        data_logs_dict["InvoiceNo"].append(line["InvoiceNo"])
-        data_logs_dict["StockCode"].append(line["StockCode"])
-        data_logs_dict["Quantity"].append(line["Quantity"])
-        data_logs_dict["InvoiceDate"].append(line["InvoiceDate"])
-        data_logs_dict["CustomerID"].append(line["CustomerID"])
-        data_logs_dict["Country"].append(line["Country"])
+with open(data_edited_csv_filepath, 'r', encoding="ISO-8859-1") as f:
+    reader = csv.DictReader(f)
 
-print('data_logs_dict["InvoiceNo"] length:', len(data_logs_dict["InvoiceNo"]))
-print('data_logs_dict["StockCode"] length:', len(data_logs_dict["StockCode"]))
-print('data_logs_dict["Quantity"] length:', len(data_logs_dict["Quantity"]))
-print('data_logs_dict["InvoiceDate"] length:', len(data_logs_dict["InvoiceDate"]))
-print('data_logs_dict["CustomerID"] length:', len(data_logs_dict["CustomerID"]))
-print('data_logs_dict["Country"] length:', len(data_logs_dict["Country"]))
+    column_names = next(reader) # Reads the first line, which contains the header
+    data_invoice = {col: [] for col in column_names}
+    for row in reader:
+        for key, value in zip(column_names, row):
+            data_invoice[key].append(value)
 
-with open(f'{logs_csv_filepath}', 'w', encoding="UTF-8", newline='') as csv_file:
+with open(invoices_csv_filepath, 'w', encoding="UTF-8", newline='') as csv_file:
     writer = csv.writer(csv_file)
-    writer.writerow(data_logs_dict.keys())
-    writer.writerows(zip(*data_logs_dict.values()))
+    writer.writerow(data_invoice.keys())
+    writer.writerows(zip(*data_invoice.values()))
--- python-scripts/create_product_info_csv_file.py
+++ python-scripts/create_product_info_csv_file.py
@@ -1,37 +1,24 @@
-"""
-The product info table will have 4 columns, StockCode, Product Description, Unit Price, and Date, the
-data will be updated once a day from HDFS. It is important to mention that the same product can be
-priced differently on different dates.
-"""
-
 import csv
+import configparser
 
-#data_edited_csv_filepath = '/home/dusan/PycharmProjects/JDP_Data_Engineering_Task/venv/csvs/data_edited2.csv'
-data_edited_csv_filepath = 'file:///home/scala/src/main/resources/csvs/data_edited.csv'
-#product_info_csv_filepath = '/home/dusan/PycharmProjects/JDP_Data_Engineering_Task/venv/csvs/product_info2.csv'
-product_info_csv_filepath = 'file:///home/scala/src/main/resources/csvs/product_info.csv'
+config = configparser.ConfigParser()
+config.read('config.ini')
 
-data_product_info_dict = {
-    "StockCode": [],
-    "Description": [],
-    "UnitPrice": [],
-    "InvoiceDate": []
-}
+data_edited_csv_filepath = config['data_edited']['path']
+product_info_csv_filepath = config['product_info']['path']
 
-with open(f'{data_edited_csv_filepath}', 'r', encoding="ISO-8859-1") as csv_file:
-    csv_reader = csv.DictReader(csv_file, delimiter=',')
-    for line in csv_reader:
-        data_product_info_dict["StockCode"].append(line["StockCode"])
-        data_product_info_dict["Description"].append(line["Description"])
-        data_product_info_dict["UnitPrice"].append(line["UnitPrice"])
-        data_product_info_dict["InvoiceDate"].append(line["InvoiceDate"])
+with open(data_edited_csv_filepath, 'r', encoding="ISO-8859-1") as f:
+    reader = csv.DictReader(f)
 
-print('data_product_info_dict["StockCode"] length:', len(data_product_info_dict["StockCode"]))
-print('data_product_info_dict["Description"] length:', len(data_product_info_dict["Description"]))
-print('data_product_info_dict["UnitPrice"] length:', len(data_product_info_dict["UnitPrice"]))
-print('data_product_info_dict["InvoiceDate"] length:', len(data_product_info_dict["InvoiceDate"]))
+    column_names = next(reader) # Reads the first line, which contains the header
+    data_product = {col: [] for col in column_names}
+    for row in reader:
+        for key, value in zip(column_names, row):
+            data_product[key].append(value)
 
-with open(f'{product_info_csv_filepath}', 'w', encoding="UTF-8", newline='') as csv_file:
+with open(product_info_csv_filepath, 'w', encoding="UTF-8", newline='') as csv_file:
     writer = csv.writer(csv_file)
-    writer.writerow(data_product_info_dict.keys())
-    writer.writerows(zip(*data_product_info_dict.values()))
+    writer.writerow(data_product.keys())
+    writer.writerows(zip(*data_product.values()))
+
+

bac9d7b17b9396f53f9519b8984573f8082fca50
dusannistoran <dulekiki@gmail.com>
2022-11-22 11:07:24 +0100

    adding try/catch blocks

--- src/main/scala/com/example/CountriesFromHDFSToHive.scala
+++ src/main/scala/com/example/CountriesFromHDFSToHive.scala
@@ -2,6 +2,7 @@ package com.example
 
 import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.hadoop.fs.{FileSystem, Path}
+import org.apache.hadoop.conf.Configuration
 import org.apache.spark.sql.{Row, SaveMode, SparkSession}
 import org.apache.spark.sql.functions.expr
 import org.apache.spark.sql.types.{StringType, StructField, StructType}
@@ -47,46 +48,61 @@ class CountriesFromHDFSToHive(hdfsPath: String, hiveTablePath: String) {
 
   def readFromHDFSAndInsertNewCountriesToHiveTable(countryHiveTable: String): Unit = {
 
-    val today = new SimpleDateFormat("dd-MM-yyyy").format(new Date())
-    println("hdfsPath: " + hdfsPath)
-    val hdfsAbsolutePath = hdfsPath + "/" + today
-    println("hdfsAbsolutePath: " + hdfsAbsolutePath)
+    try {
+      val today = new SimpleDateFormat("dd-MM-yyyy").format(new Date())
+      println("hdfsPath: " + hdfsPath)
+      val hdfsAbsolutePath = hdfsPath + "/" + today
+      println("hdfsAbsolutePath: " + hdfsAbsolutePath)
 
-    val dfCountriesFromHDFS = spark
-      .read
-      .format("csv")
-      .option("header", "true")
-      .schema(schema)
-      .load(hdfsPath)
+      val dfCountriesFromHDFS = spark
+        .read
+        .format("csv")
+        .option("header", "true")
+        .schema(schema)
+        .load(hdfsPath)
 
-    val dfCountriesFromHDFSDistinct = dfCountriesFromHDFS.distinct()
-    println("dfCountriesFromHDFSDistinct:")
-    dfCountriesFromHDFSDistinct.show()
-    println("dfCountriesFromHDFSDistinct count: " + dfCountriesFromHDFSDistinct.count())
+      val dfCountriesFromHDFSDistinct = dfCountriesFromHDFS.distinct()
+      println("dfCountriesFromHDFSDistinct:")
+      dfCountriesFromHDFSDistinct.show()
+      println("dfCountriesFromHDFSDistinct count: " + dfCountriesFromHDFSDistinct.count())
 
-    println("hiveTablePath: " + hiveTablePath)
-    spark.catalog.refreshTable("country")
+      println("hiveTablePath: " + hiveTablePath)
+      spark.catalog.refreshTable("country")
 
-    // reading country Hive table
-    val originalHiveCountriesTable = spark.read
-      .format("parquet")
-      .schema(schema)
-      .load(hiveTablePath)
+      // reading country Hive table
+      val originalHiveCountriesTable = spark.read
+        .format("parquet")
+        .schema(schema)
+        .load(hiveTablePath)
 
-    println("originalHiveCountriesTable:")
-    originalHiveCountriesTable.show()
+      println("originalHiveCountriesTable:")
+      originalHiveCountriesTable.show()
 
-    // getting the difference between new countries and countries already stored in country Hive table
-    val onlyNewCountries = dfCountriesFromHDFSDistinct
-      .join(originalHiveCountriesTable, Seq("country_id"), "leftanti")
-    println("onlyNewCountries:")
-    onlyNewCountries.show()
+      // getting the difference between new countries and countries already stored in country Hive table
+      val onlyNewCountries = dfCountriesFromHDFSDistinct
+        .join(originalHiveCountriesTable, Seq("country_id"), "leftanti")
+      println("onlyNewCountries:")
+      onlyNewCountries.show()
 
-    // appending the difference into country Hive table
-    onlyNewCountries.write
-      .mode(SaveMode.Append)
-      .format("parquet")
-      .saveAsTable(countryHiveTable)
+      // appending the difference into country Hive table
+      onlyNewCountries.write
+        .mode(SaveMode.Append)
+        .format("parquet")
+        .saveAsTable(countryHiveTable)
+
+      val fileSystem: FileSystem = {
+        val conf = new Configuration()
+        conf.set("fs.defaultFS", hdfsPath)
+        FileSystem.get(conf)
+      }
+      println("products hdfsPath: " + hdfsPath)
+      val srcPath = new Path(hdfsPath)
+      if (fileSystem.exists(srcPath)) fileSystem.delete(srcPath, true)
+    } catch {
+      case e: Exception => println("CountriesFromHDFSToHive, " +
+        "def readFromHDFSAndInsertNewCountriesToHiveTable(countryHiveTable: String): Unit, " +
+        "error occurred: " + e)
+    }
   }
 }
 
@@ -94,17 +110,23 @@ object CountriesFromHDFSToHive {
 
   def main(args: Array[String]): Unit = {
 
-    val configHDFS: Config = ConfigFactory.load().getConfig("application.hdfs")
-    val configHive: Config = ConfigFactory.load().getConfig("application.hive")
-    val hdfsCountriesPath: String = configHDFS.getString("hdfsCountriesPath")
-    val hiveTablesPathPrefix: String = configHive.getString("hiveTablesPathPrefix")
-    val hiveCountriesTableName: String = configHive.getString("countriesTableName")
-    val hiveCountriesTableWholePath = hiveTablesPathPrefix + hiveCountriesTableName
+    try {
+      val configHDFS: Config = ConfigFactory.load().getConfig("application.hdfs")
+      val configHive: Config = ConfigFactory.load().getConfig("application.hive")
+      val hdfsCountriesPath: String = configHDFS.getString("hdfsCountriesPath")
+      val hiveTablesPathPrefix: String = configHive.getString("hiveTablesPathPrefix")
+      val hiveCountriesTableName: String = configHive.getString("countriesTableName")
+      val hiveCountriesTableWholePath = hiveTablesPathPrefix + hiveCountriesTableName
 
-    val today = new SimpleDateFormat("dd-MM-yyyy").format(new Date())
-    val hdfsAbsolutePath: String = hdfsCountriesPath + "/" + today
+      val today = new SimpleDateFormat("dd-MM-yyyy").format(new Date())
+      val hdfsAbsolutePath: String = hdfsCountriesPath + "/" + today
 
-    val countriesFromHDFSToHive = new CountriesFromHDFSToHive(hdfsAbsolutePath, hiveCountriesTableWholePath)
-    countriesFromHDFSToHive.readFromHDFSAndInsertNewCountriesToHiveTable(hiveCountriesTableName)
+      val countriesFromHDFSToHive = new CountriesFromHDFSToHive(hdfsAbsolutePath, hiveCountriesTableWholePath)
+      countriesFromHDFSToHive.readFromHDFSAndInsertNewCountriesToHiveTable(hiveCountriesTableName)
+    } catch {
+      case e: Exception => println("CountriesFromHDFSToHive, " +
+        "def main(args: Array[String]): Unit, " +
+        "error occurred: " + e)
+    }
   }
 }

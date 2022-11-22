35e87835bdad68521b84671ddaa0536a83d15f23
dusannistoran <dulekiki@gmail.com>
2022-11-22 10:57:59 +0100

    leaving com.example.Std out

--- bash-scripts/test.sh
+++ bash-scripts/test.sh
@@ -11,16 +11,16 @@ class_countries2="com.example.CountriesFromHDFSToHive"
 class_invoices1="com.example.InvoicesFromLocalToHDFS"
 class_invoices2="com.example.InvoicesFromHDFSToHive"
 class_join="com.example.Join"
-class_email="com.example.Std"
-master=local[4]
+# class_email="com.example.Std"
+master=local[2]
 end=/home/scala/target/scala-2.12/jdp_2.12-0.1.0-SNAPSHOT.jar
 
-echo "10 9 * * * $spark_submit --jars $jars --class $class_products1 --master $master $end" > /var/spool/cron/crontabs/root 
-echo "11 9 * * * $spark_submit --jars $jars --class $class_products2 --master $master $end" >> /var/spool/cron/crontabs/root
-echo "12 9 * * * $spark_submit --jars $jars --class $class_countries1 --master $master $end" >> /var/spool/cron/crontabs/root
-echo "13 9 * * * $spark_submit --jars $jars --class $class_countries2 --master $master $end" >> /var/spool/cron/crontabs/root
-echo "15 * * * * $spark_submit --jars $jars --class $class_invoices1 --master $master $end" >> /var/spool/cron/crontabs/root
-echo "17 */2 * * * $spark_submit --jars $jars --class $class_invoices2 --master $master $end" >> /var/spool/cron/crontabs/root 
-echo "20 */2 * * * $spark_submit --packages $packages --jars $jars --class $class_join --master $master $end" >> /var/spool/cron/crontabs/root
-echo "25 */2 * * * $spark_submit --packages $packages --jars $email_jars --class $class_email --master $master $end" >> /var/spool/cron/crontabs/root
+echo "50 8 * * * $spark_submit --jars $jars --class $class_products1 --master $master $end" > /var/spool/cron/crontabs/root 
+echo "51 8 * * * $spark_submit --jars $jars --class $class_products2 --master $master $end" >> /var/spool/cron/crontabs/root
+echo "52 8 * * * $spark_submit --jars $jars --class $class_countries1 --master $master $end" >> /var/spool/cron/crontabs/root
+echo "53 8 * * * $spark_submit --jars $jars --class $class_countries2 --master $master $end" >> /var/spool/cron/crontabs/root
+echo "54 * * * * $spark_submit --jars $jars --class $class_invoices1 --master $master $end" >> /var/spool/cron/crontabs/root
+echo "56 */4 * * * $spark_submit --jars $jars --class $class_invoices2 --master $master $end" >> /var/spool/cron/crontabs/root 
+echo "58 */4 * * * $spark_submit --packages $packages --jars $email_jars --class $class_join --master $master $end" >> /var/spool/cron/crontabs/root
+# echo "55 */4 * * * $spark_submit --packages $packages --jars $email_jars --class $class_email --master $master $end" >> /var/spool/cron/crontabs/root
 crond start

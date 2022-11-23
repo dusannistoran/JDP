3605abc7363cb299850dcfd2972e353c66e5ba75
dusannistoran <dulekiki@gmail.com>
2022-11-23 12:47:39 +0100

    adding try/catch blocks

--- src/main/scala/com/example/InvoicesFromHDFSToHive.scala
+++ src/main/scala/com/example/InvoicesFromHDFSToHive.scala
@@ -57,72 +57,79 @@ class InvoicesFromHDFSToHive(hdfsPath: String, hiveTableName: String) {
 
   def getDataframeFromHDFSByGivenHours(nowHours: String): Unit = {
 
-    val nowHoursStr: String = getNowHoursUTC
-    println("now hours (String): " + nowHoursStr)
-    val nowHoursInt: Int = nowHoursStr.toInt
-    println("now hours (Int): " + nowHoursInt)
-    val threeHoursAgoInt: Int = nowHoursInt - 3
-    val twoHoursAgoInt: Int = nowHoursInt - 2
-    val hourAgoInt: Int = nowHoursInt - 1
-    //val threeHoursAgoStr: String = threeHoursAgoInt + ""
-    //val twoHoursAgoStr: String = twoHoursAgoInt + ""
-    //val hourAgoStr: String = hourAgoInt + ""
+    try {
+      val nowHoursStr: String = getNowHoursUTC
+      println("now hours (String): " + nowHoursStr)
+      val nowHoursInt: Int = nowHoursStr.toInt
+      println("now hours (Int): " + nowHoursInt)
+      val threeHoursAgoInt: Int = nowHoursInt - 3
+      val twoHoursAgoInt: Int = nowHoursInt - 2
+      val hourAgoInt: Int = nowHoursInt - 1
+      //val threeHoursAgoStr: String = threeHoursAgoInt + ""
+      //val twoHoursAgoStr: String = twoHoursAgoInt + ""
+      //val hourAgoStr: String = hourAgoInt + ""
 
-    val fileSystem: FileSystem = {
-      val conf = new Configuration()
-      conf.set("fs.defaultFS", hdfsPath)
-      FileSystem.get(conf)
+      val fileSystem: FileSystem = {
+        val conf = new Configuration()
+        conf.set("fs.defaultFS", hdfsPath)
+        FileSystem.get(conf)
+      }
+
+      // Getting all files names on HDFS
+      val files = fileSystem.listFiles(new Path(hdfsPath), true)
+      val filenames = ListBuffer[String]()
+      val csvFilenames = filenames.filter(filename => filename.endsWith(".csv"))
+      while (files.hasNext) filenames += files.next().getPath.toString
+      filenames.foreach(println(_))
+      println("filenames size: " + filenames.size)
+      println("csvFilenames size: " + csvFilenames.size)
+
+      // Filtering only .csv files
+      filenames.filter(filename => filename.endsWith(".csv")).foreach({ file =>
+
+        // reading each .csv file
+        val dfFromHDFS = spark
+          .read
+          .format("csv")
+          .option("header", "true")
+          .schema(invoicesSchema)
+          .load(file)
+
+        println("dfFromHDFS:")
+        dfFromHDFS.show()
+
+        println("dfFromHDFS count: " + dfFromHDFS.count())
+        println("dfFromHDFS schema:")
+        dfFromHDFS.printSchema()
+
+        // and write it to main Hive table
+        dfFromHDFS.write
+          .mode(SaveMode.Append)
+          .saveAsTable(hiveTableName)
+      })
+
+      println("hours strings:")
+      val hoursStrings = filenames.map({ filename =>
+        val filenameStrings = filename.split("/")
+        val filenameNumHoursStr = filenameStrings(6)
+        filenameNumHoursStr
+      })
+      val hoursStringsSet = hoursStrings.toSet
+      println("hoursStringsSet:")
+      hoursStringsSet.foreach(println)
+
+      // deleting all files on HDFS; they are represented by hour
+      hoursStringsSet.foreach({ hour =>
+        val hdfsAbsolutePath = hdfsPath + "/" + hour
+        val srcPath = new Path(hdfsAbsolutePath)
+        if (fileSystem.exists(srcPath)) fileSystem.delete(srcPath, true)
+      })
+    } catch {
+      case e: Exception =>
+        println("InvoicesFromHDFSToHive, " +
+          "def getDataframeFromHDFSByGivenHours(nowHours: String): Unit, " +
+          "error occurred: " + e)
     }
-
-    // Getting all files names on HDFS
-    val files = fileSystem.listFiles(new Path(hdfsPath), true)
-    val filenames = ListBuffer[String]()
-    val csvFilenames = filenames.filter(filename => filename.endsWith(".csv"))
-    while (files.hasNext) filenames += files.next().getPath.toString
-    filenames.foreach(println(_))
-    println("filenames size: " + filenames.size)
-    println("csvFilenames size: " + csvFilenames.size)
-
-    // Filtering only .csv files
-    filenames.filter(filename => filename.endsWith(".csv")).foreach({ file =>
-
-      // reading each .csv file
-      val dfFromHDFS = spark
-        .read
-        .format("csv")
-        .option("header", "true")
-        .schema(invoicesSchema)
-        .load(file)
-
-      println("dfFromHDFS:")
-      dfFromHDFS.show()
-
-      println("dfFromHDFS count: " + dfFromHDFS.count())
-      println("dfFromHDFS schema:")
-      dfFromHDFS.printSchema()
-
-      // and write it to main Hive table
-      dfFromHDFS.write
-        .mode(SaveMode.Append)
-        .saveAsTable(hiveTableName)
-    })
-
-    println("hours strings:")
-    val hoursStrings = filenames.map({filename =>
-      val filenameStrings = filename.split("/")
-      val filenameNumHoursStr = filenameStrings(6)
-      filenameNumHoursStr
-    })
-    val hoursStringsSet = hoursStrings.toSet
-    println("hoursStringsSet:")
-    hoursStringsSet.foreach(println)
-
-    // deleting all files on HDFS; they are represented by hour
-    hoursStringsSet.foreach({ hour =>
-      val hdfsAbsolutePath = hdfsPath + "/" + hour
-      val srcPath = new Path(hdfsAbsolutePath)
-      if (fileSystem.exists(srcPath)) fileSystem.delete(srcPath, true)
-    })
   }
 }
 
@@ -130,15 +137,21 @@ object InvoicesFromHDFSToHive {
 
   def main(args: Array[String]): Unit = {
 
-    val configHDFS: Config = ConfigFactory.load().getConfig("application.hdfs")
-    val configHive: Config = ConfigFactory.load().getConfig("application.hive")
-    val hdfsInvoicesPath: String = configHDFS.getString("hdfsInvoicesPath")
-    //val hiveTablesPathPrefix = configHive.getString("hiveTablesPathPrefix")
-    val hiveMainTableName = configHive.getString("invoicesTableName")
+    try {
+      val configHDFS: Config = ConfigFactory.load().getConfig("application.hdfs")
+      val configHive: Config = ConfigFactory.load().getConfig("application.hive")
+      val hdfsInvoicesPath: String = configHDFS.getString("hdfsInvoicesPath")
+      //val hiveTablesPathPrefix = configHive.getString("hiveTablesPathPrefix")
+      val hiveMainTableName = configHive.getString("invoicesTableName")
 
-    val invoicesFromHDFSToHive = new InvoicesFromHDFSToHive(hdfsInvoicesPath, hiveMainTableName)
-    val nowHours: String = getNowHoursUTC
-    invoicesFromHDFSToHive.getDataframeFromHDFSByGivenHours(nowHours)
-    println("hdfsPath: " + hdfsInvoicesPath)
+      val invoicesFromHDFSToHive = new InvoicesFromHDFSToHive(hdfsInvoicesPath, hiveMainTableName)
+      val nowHours: String = getNowHoursUTC
+      invoicesFromHDFSToHive.getDataframeFromHDFSByGivenHours(nowHours)
+      println("hdfsPath: " + hdfsInvoicesPath)
+    } catch {
+      case e: Exception => println("InvoicesFromHDFSToHive, " +
+        "def main(args: Array[String]): Unit, " +
+        "error occurred: " + e)
+    }
   }
 }

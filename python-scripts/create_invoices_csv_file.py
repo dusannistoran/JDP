3668294034b5bccd38e4662dd1cc8a7fa6aae1b5
dusannistoran <dulekiki@gmail.com>
2022-11-24 09:34:08 +0100

    adding try/except blocks

--- python-scripts/create_invoices_csv_file.py
+++ python-scripts/create_invoices_csv_file.py
@@ -7,16 +7,22 @@ config.read('config.ini')
 data_edited_csv_filepath = config['data_edited']['path']
 invoices_csv_filepath = config['invoices']['path']
 
-with open(data_edited_csv_filepath, 'r', encoding="ISO-8859-1") as f:
-    reader = csv.DictReader(f)
+try:
+    with open(data_edited_csv_filepath, 'r', encoding="ISO-8859-1") as f:
+        reader = csv.DictReader(f)
 
-    column_names = next(reader) # Reads the first line, which contains the header
-    data_invoice = {col: [] for col in column_names}
-    for row in reader:
-        for key, value in zip(column_names, row):
-            data_invoice[key].append(value)
+        column_names = next(reader) # Reads the first line, which contains the header
+        data_invoice = {col: [] for col in column_names}
+        for row in reader:
+            for key, value in zip(column_names, row):
+                data_invoice[key].append(value)
+except:
+    print("An exception occurred when opening input file and creating the invoices dictionary.")
 
-with open(invoices_csv_filepath, 'w', encoding="UTF-8", newline='') as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerow(data_invoice.keys())
-    writer.writerows(zip(*data_invoice.values()))
+try:
+    with open(invoices_csv_filepath, 'w', encoding="UTF-8", newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(data_invoice.keys())
+        writer.writerows(zip(*data_invoice.values()))
+except:
+    print("An exception occurred when writing invoices dictionary into output file.")

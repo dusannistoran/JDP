ffc4bf16d869a9a87d4e19f4861bffa057059f4b
dusannistoran <dulekiki@gmail.com>
2022-11-24 11:05:50 +0100

    adding try/catch blocks

--- src/main/scala/com/example/Queries.scala
+++ src/main/scala/com/example/Queries.scala
@@ -2,6 +2,7 @@ package com.example
 
 import com.typesafe.config.{Config, ConfigFactory}
 import org.apache.spark.sql.functions.{asc, col, count, countDistinct, desc, sum}
+import org.apache.spark.sql.types.StructType
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.slf4j.LoggerFactory
 
@@ -41,17 +42,27 @@ class Queries {
   LoggerFactory.getLogger(spark.getClass)
   spark.sparkContext.setLogLevel("WARN")
 
-  def getDataframeFromPostgres(): DataFrame = {
+  def getDataframeFromPostgres: DataFrame = {
 
-    val df = spark.read
-      .format("jdbc")
-      .option("driver", s"$postgresDriver")
-      .option("url", s"$postgresUrl")
-      .option("dbtable", "joined")
-      .option("user", s"$postgresUser")
-      .option("password", s"$postgresPassword")
-      .load()
-    df
+    try {
+      val df = spark.read
+        .format("jdbc")
+        .option("driver", s"$postgresDriver")
+        .option("url", s"$postgresUrl")
+        .option("dbtable", "joined")
+        .option("user", s"$postgresUser")
+        .option("password", s"$postgresPassword")
+        .load()
+      df
+    } catch {
+      case e: Exception => println("Queries, " +
+        "def getDataframeFromPostgres: DataFrame, " +
+        "error occurred: " + e)
+        import org.apache.spark.sql.Row
+        val emptyDf: DataFrame = spark.createDataFrame(spark.sparkContext.emptyRDD[Row], StructType(Seq()))
+        emptyDf
+    }
+
   }
 }
 
@@ -59,93 +70,93 @@ object Queries {
 
   def main(args: Array[String]): Unit = {
 
-    val queries = new Queries()
+    try {
+      val queries = new Queries()
 
-    val dfFromPostgres: DataFrame = queries.getDataframeFromPostgres()
-    println("dfFromPostgres:")
-    dfFromPostgres.show()
-    println("dfFromPostgres count: " + dfFromPostgres.count())
-    println("dfFromPostgres schema:")
-    dfFromPostgres.printSchema()
+      val dfFromPostgres: DataFrame = queries.getDataframeFromPostgres
+      println("dfFromPostgres:")
+      dfFromPostgres.show()
+      println("dfFromPostgres count: " + dfFromPostgres.count())
+      println("dfFromPostgres schema:")
+      dfFromPostgres.printSchema()
 
-    // TOP BUYERS PER USER
-    val topBuyersPerUser = dfFromPostgres
-      .filter(col("customer_id").isNotNull)
-      .groupBy("customer_id")
-      .agg(sum("total_price") as "sum")
-      .orderBy(desc("sum"))
-    println("TOP BUYERS PER USER:")
-    topBuyersPerUser.show()
-    println("topBuyersPerUser count: " + topBuyersPerUser.count())
+      // TOP BUYERS PER USER
+      val topBuyersPerUser = dfFromPostgres
+        .filter(col("customer_id").isNotNull)
+        .groupBy("customer_id")
+        .agg(sum("total_price") as "sum")
+        .orderBy(desc("sum"))
+      println("TOP BUYERS PER USER:")
+      topBuyersPerUser.show()
+      println("topBuyersPerUser count: " + topBuyersPerUser.count())
 
+      // TOP BUYERS PER COUNTRY REGION
+      val topBuyersPerCountryRegion = dfFromPostgres
+        .filter(col("customer_id").isNotNull)
+        .groupBy("customer_id", "country_id", "country_name", "region_id")
+        .agg(sum("total_price") as "sum")
+        .orderBy(desc("sum"))
+      println("TOP BUYERS PER COUNTRY REGION:")
+      topBuyersPerCountryRegion.show()
+      println("topBuyersPerCountryRegion count: " + topBuyersPerCountryRegion.count())
 
-    // TOP BUYERS PER COUNTRY REGION
-    val topBuyersPerCountryRegion = dfFromPostgres
-      .filter(col("customer_id").isNotNull)
-      .groupBy("customer_id", "country_id", "country_name", "region_id")
-      .agg(sum("total_price") as "sum")
-      .orderBy(desc("sum"))
-    println("TOP BUYERS PER COUNTRY REGION:")
-    topBuyersPerCountryRegion.show()
-    println("topBuyersPerCountryRegion count: " + topBuyersPerCountryRegion.count())
+      // USERS WHO PLACE ORDERS FROM DIFFERENT REGIONS OF THE SAME COUNTRY
+      val aggregatingCols = Seq("customer_id", "country_id", "country_name")
+      val usersWhoPlaceOrdersFromDifferentRegionsOfTheSameCountry = dfFromPostgres
+        .filter(col("customer_id").isNotNull)
+        .groupBy(aggregatingCols.head, aggregatingCols.tail: _*)
+        .agg(countDistinct("region_id").as("distinct_regions"))
+        .select("distinct_regions", aggregatingCols: _*)
+        .join(dfFromPostgres, usingColumns = aggregatingCols)
+        .orderBy(desc("distinct_regions"))
+        .where(col("distinct_regions").gt(1))
 
+      println("USERS WHO PLACE ORDERS FROM DIFFERENT REGIONS OF THE SAME COUNTRY:")
+      usersWhoPlaceOrdersFromDifferentRegionsOfTheSameCountry.show(151)
+      println("usersWhoPlaceOrdersFromDifferentRegionsOfTheSameCountry count: " +
+        usersWhoPlaceOrdersFromDifferentRegionsOfTheSameCountry.count())
 
-    // USERS WHO PLACE ORDERS FROM DIFFERENT REGIONS OF THE SAME COUNTRY
-    val aggregatingCols = Seq("customer_id", "country_id", "country_name")
-    val usersWhoPlaceOrdersFromDifferentRegionsOfTheSameCountry = dfFromPostgres
-      .filter(col("customer_id").isNotNull)
-      .groupBy(aggregatingCols.head, aggregatingCols.tail:_*)
-      .agg(countDistinct("region_id").as("distinct_regions"))
-      .select("distinct_regions", aggregatingCols:_*)
-      .join(dfFromPostgres, usingColumns = aggregatingCols)
-      .orderBy(desc("distinct_regions"))
-      .where(col("distinct_regions").gt(1))
+      // USERS WHO ORDER FROM MORE THAN ONE COUNTRY
+      val usersWhoOrderFromMoreThanOneCountry = dfFromPostgres
+        .filter(col("customer_id").isNotNull)
+        .groupBy("customer_id")
+        .agg(countDistinct("country_name").as("distinct_countries"))
+        .select("distinct_countries", "customer_id")
+        .join(dfFromPostgres, usingColumn = "customer_id")
+        .orderBy(desc("distinct_countries"))
+        .where(col("distinct_countries").gt(1))
 
-    println("USERS WHO PLACE ORDERS FROM DIFFERENT REGIONS OF THE SAME COUNTRY:")
-    usersWhoPlaceOrdersFromDifferentRegionsOfTheSameCountry.show(151)
-    println("usersWhoPlaceOrdersFromDifferentRegionsOfTheSameCountry count: " +
-      usersWhoPlaceOrdersFromDifferentRegionsOfTheSameCountry.count())
+      println("USERS WHO ORDER FROM MORE THAN ONE COUNTRY:")
+      usersWhoOrderFromMoreThanOneCountry.show()
+      println("usersWhoOrderFromMoreThanOneCountry count: " + usersWhoOrderFromMoreThanOneCountry.count())
 
+      // HOW MANY USERS HAVE PLACED ONE ORDER
+      val usersWhoHavePlacedOneOrder = dfFromPostgres
+        .filter(col("customer_id").isNotNull)
+        .groupBy("customer_id")
+        .agg(count("*").as("cnt"))
+        .orderBy(asc("customer_id"))
+        .where(col("cnt").equalTo(1))
 
-    // USERS WHO ORDER FROM MORE THAN ONE COUNTRY
-    val usersWhoOrderFromMoreThanOneCountry = dfFromPostgres
-      .filter(col("customer_id").isNotNull)
-      .groupBy("customer_id")
-      .agg(countDistinct("country_name").as("distinct_countries"))
-      .select("distinct_countries", "customer_id")
-      .join(dfFromPostgres, usingColumn = "customer_id")
-      .orderBy(desc("distinct_countries"))
-      .where(col("distinct_countries").gt(1))
+      println("HOW MANY USERS HAVE PLACED ONE ORDER:")
+      usersWhoHavePlacedOneOrder.show()
+      println("usersWhoHavePlacedOneOrder count: " + usersWhoHavePlacedOneOrder.count())
 
-    println("USERS WHO ORDER FROM MORE THAN ONE COUNTRY:")
-    usersWhoOrderFromMoreThanOneCountry.show()
-    println("usersWhoOrderFromMoreThanOneCountry count: " + usersWhoOrderFromMoreThanOneCountry.count())
+      // HOW MANY USERS HAVE PLACED MORE THAN ONE ORDER
+      val usersWhoHavePlacedMoreThanOneOrder = dfFromPostgres
+        .filter(col("customer_id").isNotNull)
+        .groupBy("customer_id")
+        .agg(count("*").as("cnt"))
+        .orderBy(desc("cnt"), asc("customer_id"))
+        .where(col("cnt").gt(1))
 
-
-    // HOW MANY USERS HAVE PLACED ONE ORDER
-    val usersWhoHavePlacedOneOrder = dfFromPostgres
-      .filter(col("customer_id").isNotNull)
-      .groupBy("customer_id")
-      .agg(count("*").as("cnt"))
-      .orderBy(asc("customer_id"))
-      .where(col("cnt").equalTo(1))
-
-    println("HOW MANY USERS HAVE PLACED ONE ORDER:")
-    usersWhoHavePlacedOneOrder.show()
-    println("usersWhoHavePlacedOneOrder count: " + usersWhoHavePlacedOneOrder.count())
-
-
-    // HOW MANY USERS HAVE PLACED MORE THAN ONE ORDER
-    val usersWhoHavePlacedMoreThanOneOrder = dfFromPostgres
-      .filter(col("customer_id").isNotNull)
-      .groupBy("customer_id")
-      .agg(count("*").as("cnt"))
-      .orderBy(desc("cnt"), asc("customer_id"))
-      .where(col("cnt").gt(1))
-
-    println("HOW MANY USERS HAVE PLACED MORE THAN ONE ORDER:")
-    usersWhoHavePlacedMoreThanOneOrder.show()
-    println("usersWhoHavePlacedMoreThanOneOrder count: " + usersWhoHavePlacedMoreThanOneOrder.count())
-
+      println("HOW MANY USERS HAVE PLACED MORE THAN ONE ORDER:")
+      usersWhoHavePlacedMoreThanOneOrder.show()
+      println("usersWhoHavePlacedMoreThanOneOrder count: " + usersWhoHavePlacedMoreThanOneOrder.count())
+    } catch {
+      case e: Exception => println("Queries, " +
+        "def main(args: Array[String]): Unit, " +
+        "error occurred: " + e)
+    }
   }
 }

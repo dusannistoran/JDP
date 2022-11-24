89867c2ddccd75d23f182bb77994230105a66e72
dusannistoran <dulekiki@gmail.com>
2022-11-24 09:35:18 +0100

    adding try/except blocks

--- python-scripts/create_product_info_csv_file.py
+++ python-scripts/create_product_info_csv_file.py
@@ -7,18 +7,24 @@ config.read('config.ini')
 data_edited_csv_filepath = config['data_edited']['path']
 product_info_csv_filepath = config['product_info']['path']
 
-with open(data_edited_csv_filepath, 'r', encoding="ISO-8859-1") as f:
-    reader = csv.DictReader(f)
+try:
+    with open(data_edited_csv_filepath, 'r', encoding="ISO-8859-1") as f:
+        reader = csv.DictReader(f)
 
-    column_names = next(reader) # Reads the first line, which contains the header
-    data_product = {col: [] for col in column_names}
-    for row in reader:
-        for key, value in zip(column_names, row):
-            data_product[key].append(value)
+        column_names = next(reader) # Reads the first line, which contains the header
+        data_product = {col: [] for col in column_names}
+        for row in reader:
+            for key, value in zip(column_names, row):
+                data_product[key].append(value)
+except:
+    print("An exception occurred when opening input file and creating the product_info dictionary.")
 
-with open(product_info_csv_filepath, 'w', encoding="UTF-8", newline='') as csv_file:
-    writer = csv.writer(csv_file)
-    writer.writerow(data_product.keys())
-    writer.writerows(zip(*data_product.values()))
+try:
+    with open(product_info_csv_filepath, 'w', encoding="UTF-8", newline='') as csv_file:
+        writer = csv.writer(csv_file)
+        writer.writerow(data_product.keys())
+        writer.writerows(zip(*data_product.values()))
+except:
+    print("An exception occurred when writing product_info dictionary into output file.")
 
 

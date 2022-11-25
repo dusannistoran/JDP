6633a7532c3ee3c26a0ce78dc9c6e097a1e158e2
dusannistoran <dulekiki@gmail.com>
2022-11-25 09:17:31 +0100

    deleting commented lines

--- bash-scripts/test.sh
+++ bash-scripts/test.sh
@@ -11,7 +11,6 @@ class_countries2="com.example.CountriesFromHDFSToHive"
 class_invoices1="com.example.InvoicesFromLocalToHDFS"
 class_invoices2="com.example.InvoicesFromHDFSToHive"
 class_join="com.example.Join"
-# class_email="com.example.Std"
 master=local[2]
 end=/home/scala/target/scala-2.12/jdp_2.12-0.1.0-SNAPSHOT.jar
 
@@ -22,5 +21,4 @@ echo "53 8 * * * $spark_submit --jars $jars --class $class_countries2 --master $
 echo "54 * * * * $spark_submit --jars $jars --class $class_invoices1 --master $master $end" >> /var/spool/cron/crontabs/root
 echo "56 */4 * * * $spark_submit --jars $jars --class $class_invoices2 --master $master $end" >> /var/spool/cron/crontabs/root 
 echo "58 */4 * * * $spark_submit --packages $packages --jars $email_jars --class $class_join --master $master $end" >> /var/spool/cron/crontabs/root
-# echo "55 */4 * * * $spark_submit --packages $packages --jars $email_jars --class $class_email --master $master $end" >> /var/spool/cron/crontabs/root
 crond start

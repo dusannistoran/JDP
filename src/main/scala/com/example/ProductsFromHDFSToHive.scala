7b431d968605d171d555ab1beb3bc19b6b498f63
dusannistoran <dulekiki@gmail.com>
2022-12-21 09:11:43 +0100

    removing lazy from instantiating spark

--- src/main/scala/com/example/ProductsFromHDFSToHive.scala
+++ src/main/scala/com/example/ProductsFromHDFSToHive.scala
@@ -25,7 +25,7 @@ class ProductsFromHDFSToHive(hdfsPath: String, hiveTableName: String) {
 
   val warehouseLocation: String = new File("spark-warehouse").getAbsolutePath
 
-  lazy val spark: SparkSession = SparkSession
+  val spark: SparkSession = SparkSession
     .builder()
     .config("spark.speculation", "false")
     .config("checkpointLocation", s"$checkpoint")
